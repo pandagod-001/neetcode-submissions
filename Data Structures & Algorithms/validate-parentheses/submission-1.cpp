9f832059c9a6f3db694f6a3d4944e27fcb43d2e8
pandagod-001 <abhijay150@gmail.com>
2026-06-27 21:46:27 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        stack<char> brackets;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                brackets.push(c);
+            } 
+            else {
+                if (brackets.empty()) {
+                    return false;
+                }
+                
+                char open = brackets.top();
+                brackets.pop(); 
+                if ((c == ')' && open != '(') ||(c == '}' && open != '{') ||(c == ']' && open != '[')) {
+                    return false;
+                }
+            }
+        }
+        return brackets.empty();
+    }
+};
